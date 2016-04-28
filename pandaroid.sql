56f73c6c0af346acae7aced5d6ce928bb68ae3dc
toutounegaming1993 <duhesme.antoine@gmail.com>
2016-04-28 21:46:11 +0200

    update BDD

--- pandaroid.sql
+++ pandaroid.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Jeu 28 Avril 2016 à 18:14
+-- Généré le :  Jeu 28 Avril 2016 à 19:45
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -90,7 +90,9 @@ CREATE TABLE `photos` (
 
 INSERT INTO `photos` (`id`, `Nom`, `Lieu`, `Proprietaire`, `Date`) VALUES
 (1, '160428060336.PNG', 'Paris', 1, '2016-04-28'),
-(2, '160428072940.PNG', 'Paris', 1, '2016-04-28');
+(2, '160428072940.PNG', 'Paris', 1, '2016-04-28'),
+(3, '160428081941.PNG', 'Paris', 1, '2016-04-28'),
+(4, '160428093655.PNG', 'Paris', 1, '2016-04-28');
 
 --
 -- Index pour les tables exportées
@@ -143,7 +145,7 @@ ALTER TABLE `membre`
 -- AUTO_INCREMENT pour la table `photos`
 --
 ALTER TABLE `photos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
